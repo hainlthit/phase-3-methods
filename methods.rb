095bc2c20987f4360f164e056ac1d7f705cc156f
Hain Thit <hainlthit@gmail.com>
2022-01-30 21:24:14 -0500

    commit tree 751328f3dd38dfa268ac4bc0c0a1eb74e2699be1

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!" 
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end 
+
+greet "Naureen"
+greet "Jimmy"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end 
+
+greet_with_default
+greet_with_default "Naureen"
+
+def add(num1 , num2)
+    return num1 + num2
+end 
+
+def halve i = 1
+    if i.class != Integer
+        return nil
+    end
+    
+  i.div(2)
+   
+end
+
